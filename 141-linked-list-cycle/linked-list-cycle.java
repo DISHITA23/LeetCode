1d0c93f0c478cb08cf4fa62c3dd972bd8a94d90c
DISHITA23 <dishitamodi04@gmail.com>
2026-03-28 22:30:24 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (90.51%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slowptr=head;
+        ListNode fastptr=head;
+
+        while(slowptr!=null && fastptr!=null && fastptr.next!=null){
+        slowptr=slowptr.next;
+        fastptr=fastptr.next.next;
+
+        if(slowptr==fastptr)
+        {
+            return true;
+        }
+        }
+        return false;
+    }
+}
